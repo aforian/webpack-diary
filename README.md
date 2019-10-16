ff2c66517738316b8c6b2d1c5d19f879507ea0c9
aforian <alexian853@gmail.com>
2019-10-16 14:59:14 +0800

    day2 module and loader

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ diary for learning webpack
 - webpack.config.js 基本構成（entry, output, devServer, plugins）
 - 常用的數個plugins
 - 使用localhost啟用虛擬伺服器環境和hot-reload
+
+## Day 2 result
+- webpack.config.js 其它常用屬性（devtool，module）
+- CSS及其它檔案類型透過設定module的rules配置loader並進行編譯和打包
+- 分離提取CSS的方法
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import './index.css'
+
 function initBody() {
   var element = document.createElement('div');
-  var msg = 'Hello World!!'+ process.env.NODE_ENV;
+  var msg = 'Hello World!!';
 
   element.innerHTML = msg;
   console.log(msg)
+  console.log(`env: ${process.env.NODE_ENV}`)
 
   return element;
 }
